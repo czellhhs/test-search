c1a381a14d53b036ffa542c341eebf01f9f58b23
Christopher Zell <czell@revacomm.com>
2024-02-09 15:12:53 -0500

    initial

--- README.md
+++ README.md
@@ -1,46 +1,16 @@
-# Getting Started with Create React App
+# Local tester for ElasticSearch
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1. Have access to the CCG website - `https://github.com/CMS-Enterprise/ccg-redev-ui`
+2. Get Docker installed
+3. With Docker installed, do a:
+```
+docker run --rm --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "xpack.security.enabled=false" -e 'http.cors.allow-origin=http://localhost:3000' -e "http.cors.enabled=true" -e "http.cors.allow-headers=X-Requested-With,X-Auth-Token,Content-Type,Content-Length,Authorization" -e "http.cors.allow-credentials=true"  elasticsearch:8.12.1
+```
+which runs ElasticSearch in a docker container locally - with NO security and CORS basically wide-open - for testing.
 
-## Available Scripts
+4. Run the updateSearch script from CCG project from the root of that project.  (Read that script's header comments for proper usage and setup).
+5. From the root of this project, then do an `npm install`.
+6. Then do an `npm start`... the running Docker ES instance lets client from localhost:3000 query with no CORS issues.  So make sure you're running this project on 3000.
+7. Interact with the UI (explore the search API messages on the Dev Console etc).
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+Note: Once you stop the Docker (with a Ctrl+C [unless you run it headless,detached]) you'll need to re-run the search updater script (most likely).
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+interface SearchResults {
+  hits: {
+    total: { value: number };
+    hits: any[];
+  };
+}
+const searchEs = async (searchTerm: string): Promise<SearchResults> => {
+  const resp = await fetch(
+    "http://localhost:9200/pages/_search?q=" + searchTerm,
+    {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        highlight: {
+          // text chunk size with match
+          fragment_size: 150,
+          fields: {
+            // allfields
+            "*": {},
+          },
+        },
+      }),
+    }
+  );
+  const json = await resp.json();
+  return json;
+};
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [results, setResults] = useState<SearchResults | undefined>(undefined);
+  const timeoutHdl = useRef<any>(null);
+
+  useEffect(() => {
+    if (!searchValue?.trim()) {
+      setResults(undefined);
+      return;
+    }
+    if (timeoutHdl.current !== null) {
+      clearTimeout(timeoutHdl.current);
+    }
+
+    // settle time of 1sec before searching
+    timeoutHdl.current = setTimeout(async () => {
+      setResults(await searchEs(searchValue));
+    }, 1000);
+
+    // on umount - clear any pending timers
+    return () => {
+      if (timeoutHdl.current !== null) {
+        clearTimeout(timeoutHdl.current);
+      }
+    };
+  }, [searchValue]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="search"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </form>
+      <div>{`${results?.hits?.total?.value} hits` ?? null}</div>
+      <div className="search-results">
+        {/* Display results... since we want highlighted results, and ES embeds <em/> tags for the
+        highlighted hits, we need to use a cheap and easy way to honor that styling... hence dangerouslySetInnerHTML :( */}
+        {results?.hits.hits.map((h) => (
+          <span style={{ display: "flex", flexDirection: "column" }}>
+            {/* Link the title of the article to the full slug */}
+            <a href={h._source.fullSlug}>
+              <span
+                className="search-result-title"
+                dangerouslySetInnerHTML={{
+                  // use the matched/highlight title if there was indeed a match there
+                  // or just fallback to the regular, non-highlighted title
+                  __html: h.highlight.title ?? h._source.title,
+                }}
+              />
+            </a>
+            {/* For each highlighted match found in the `text` field... display as own 'chunk' */}
+            {h.highlight.text.map((match: any) => (
+              <span
+                className="search-result-text"
+                dangerouslySetInnerHTML={{ __html: `${match}...` }}
+              />
+            ))}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
